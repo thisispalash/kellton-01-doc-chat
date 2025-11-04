0c072939c6f0731e006cd8126ecf0085f0f7ca60
Palash A <palash.agarwal.96@gmail.com>
2025-11-04 05:39:45 -0500

    allow unsafe

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -60,7 +60,7 @@ def create_app(config_class=Config):
 def run_app():
     """Run the application with SocketIO."""
     app = create_app()
-    socketio.run(app, host='0.0.0.0', port=5001, debug=app.config['DEBUG'])
+    socketio.run(app, host='0.0.0.0', port=5001, debug=app.config['DEBUG'], allow_unsafe_werkzeug=True)
 
 
 if __name__ == '__main__':
